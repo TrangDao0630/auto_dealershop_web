a1220b269ceadcb5a24b336cc9f66d061dd7852f
TrangDao0630 <thutrangtt00@gmail.com>
2025-11-29 22:04:30 -0800

    fix UI layout and navigation links

--- landing_page.js
+++ landing_page.js
@@ -4,11 +4,10 @@
 const menuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
 
-// Add a click event listener to the button
+// Click event listener to the button
 menuButton.addEventListener('click', () => {
     // Toggle the 'hidden' class on the menu
     mobileMenu.classList.toggle('hidden');
 });
 
-// You can add more JavaScript functionality here.
 console.log("AutoHub Motors script loaded!");
